bbba9da39144657085a52ee919676c0341865b80
Bicry Aguillon <bmaguillon2@gmail.com>
2022-03-28 14:30:39 -0500

    added file_11 to easy_9

--- small_problems/easy_9/file_11.rb
+++ small_problems/easy_9/file_11.rb
@@ -2,12 +2,18 @@ words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
 	'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
 	'flow', 'neon']
 
-def anagram_outputter(arr)
-	#
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
 end
 
-def anagram?(str1, str2)
-	str1.chars.sort.to_s == str2.chars.sort.to_s
+result.each_value do |v|
+  puts "------"
+  p v
 end
-
-p anagram?("neon", "none")
